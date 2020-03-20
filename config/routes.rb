55ace9188f8a07cb9b342238f34694e86aa29ae3
西田俊陽 <toshi@toshinoMacBook-ea.local>
2020-03-20 17:16:16 +0900

    Add routes search

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "boards#index"
-  resources :boards
+  resources :boards do
+    collection do
+      get 'search'
+    end
+  end
   resources :comments, only: [:create]
 end
